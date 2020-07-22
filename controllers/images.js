f89337c42884129c63aa3db940b59b9410e00c09
linhbui98 <linhbuiquang2321998@gmail.com>
2020-07-22 16:00:32 +0700

    test

--- controllers/images.js
+++ controllers/images.js
@@ -1,6 +1,5 @@
 module.exports = {
     getImages: (req, res) => {
-        const host = req.headers.host;
         res.render('index', {
             title: 'HTTP/1.x',
         })
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const Express = require('express');
-const Https = require('http');
+const Http = require('http');
 const path = require('path')
 const indexRoute = require('./routes/index');
 
@@ -10,6 +10,6 @@ http1app.set('views', path.join(__dirname, 'views'));
 http1app.set('view engine', 'pug');
 http1app.get('/', indexRoute)
 
-Https.createServer(http1app).listen(process.env.PORT || 8080, () => {
+Http.createServer(http1app).listen(process.env.PORT || 8080, () => {
     console.log("App running")
 });
